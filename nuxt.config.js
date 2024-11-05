6bf31983f1a037b65fca3cf5347e3a28f0183f2b
Lucas Spitzer <lucasspitzer30@gmail.com>
2024-11-05 14:57:10 +0100

    Installing NuxtUI

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: "static",
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
+	target: "static",
+	compatibilityDate: "2024-04-03",
+	devtools: { enabled: true },
 
-  css: [
-    "bootstrap/dist/css/bootstrap.css"
-  ],
+	css: ["bootstrap/dist/css/bootstrap.css"],
 
-  build: {
-    transpile: ["bootstrap-vue-next"],
-  },
+	build: {
+		transpile: ["bootstrap-vue-next"],
+	},
 
-  modules: ["@nuxt/icon"]
-})
+	modules: ["@nuxt/icon"],
+});
