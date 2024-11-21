6e6467f30d4337be875429b8a8e98bdb3d982a19
Lucas Spitzer <lucasspitzer30@gmail.com>
2024-11-21 21:25:41 +0100

    Adding Vercel Web Analytics support

--- app.vue
+++ app.vue
@@ -1,8 +1,10 @@
 <template>
 	<NuxtPage />
+	<Analytics />
 </template>
 
 <script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
 useHead({
 	title: "KIT de sensibilisation au IA",
 	meta: [{ name: "description", content: "Ce site a pour but de vous fournir différentes ressources pour mieux appréhender les intelligences artificielles." }],
